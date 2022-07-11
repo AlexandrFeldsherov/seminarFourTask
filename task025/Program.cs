9479f5687d652b50cbf631da2075291415711fe5
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-07-11 09:57:58 +0300

    V004

--- task025/Program.cs
+++ task025/Program.cs
@@ -2,19 +2,55 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
-int MP(int num, int p)
+double MP(int num, int p)// возведение в степень
 {
-    int rezult = 1;
-    for (int i = 1; i <= p; i++)
+    double rezult = 1;
+    int pNew = Math.Abs(p);
+    for (int i = 1; i <= pNew; i++)
     {
         rezult = rezult * num;
     }
-    return rezult;
+    return p < 0 ? 1 / rezult : rezult;
 }
+int CheckingNumberInput(string numNew)// проверка введено ли число выводит int
+{
+    bool DeterminingNumber(string num)//проверка введены ли числа
+    {
+        foreach (var item in num)
+        {
+            if (!char.IsDigit(item))
+                return false;
+        }
+        return true;
+    }
+    string TrRe(string num)// удаляет в начале и в конце строки (-),( ),(,),(.) и внутри меняет запятую на String.Empty
+    {
+        char[] charsToTrim = { '-', '-', '-', ' ', '\'' };//Для Trim, что надо удалить в начале и в конце
+        num = num.Trim(charsToTrim);
+        return num;
+    }
+    string num = TrRe(numNew);
+    while (true)
+    {
+        if (!String.IsNullOrEmpty(num) && DeterminingNumber(num))//проверка на NULL && проверка введено ли число
+        {
+            break;
+        }
+        Console.Write("Неверный ввод, попробуйте еще раз: ");
+        num = Console.ReadLine();
+        numNew = num;
+    }
+    return Convert.ToInt32(numNew);
 
+}
+Console.Clear();
 Console.Write("Введите  число : ");
-int numFirst = Convert.ToInt32(Console.ReadLine());
+string numFirst = Console.ReadLine();
+int numFirstNew = CheckingNumberInput(numFirst);
 Console.Write("Введите  степень, в которую возвести число, введенное ранее : ");
-int numSecond = Convert.ToInt32(Console.ReadLine());
-decimal rezult = MP(numFirst, numSecond);
-Console.Write("{0}, {1} -> {2}", numFirst, numSecond, rezult);
+string numSecond = Console.ReadLine();
+int numSecondNew = CheckingNumberInput(numSecond);
+double rezult = MP(numFirstNew, numSecondNew);
+if (numFirstNew == 0 && numSecondNew == 0) Console.Write("{0}, {1} -> значение не определено", numFirstNew, numSecondNew);
+else Console.Write("{0}, {1} -> {2}", numFirstNew, numSecondNew, rezult);
+
